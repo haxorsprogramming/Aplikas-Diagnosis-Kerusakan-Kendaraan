6fbb015ffbbc87cea7bae34e1fa2cde6a7738846
haxorsprogramming <haxorsuinsu@gmail.com>
2020-02-02 22:36:43 +0700

    untuk diri sendiri

---
+++ platforms/browser/www/login.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $('#frm_login').hide();
+    let statusKoneksi = navigator.onLine;
+
+    if(statusKoneksi === true){
+        $('#divStatServer').html('Status koneksi ke server : terhubung');
+         }else{
+           $('#divStatServer').html('Status koneksi ke server : tidak terhubung. Harap periksa koneksi');
+         }
+
+    $('#btnMasukAplikasi').click(function(){
+        $('#frm_pelanggan').hide();
+        $('#frm_login').show();
+        $('#txtUsername').focus();
+    });
+
+    $('#btnMasuk').click(function(){
+        let username = $('#txtUsername').val();
+        let password = $('#txtPassword').val();
+        let dataSend = {'username':username, 'password':password}
+        $('#capNotifLogin').html("Login ke sistem ...");
+        $.post('http://api.haxors.or.id/riyan/login_cek.php',dataSend,function(data){
+            let obj = JSON.parse(data);
+            if(obj.status === 'sukses'){                
+                aksi_sukses();
+            }else{
+                aksi_gagal();
+            }
+        });
+    });
+
+    function aksi_gagal(){
+        iziToast.show({
+            title: 'Gagal',
+            message: 'Login gagal, periksa username & password!!!',
+            position: 'bottomCenter',
+            timeout: 2000,
+            pauseOnHover : false,
+            onClosed: function () {
+                $('#capNotifLogin').html("");
+                $('#txtUsername').val("");
+                $('#txtPassword').val("");
+                $('#txtUsername').focus();
+            } 
+          });
+    }
+    
+    function aksi_sukses(){
+        iziToast.show({
+            title: 'Sukses',
+            message: 'Anda berhasil login',
+            position: 'bottomCenter',
+            timeout: 3000,
+            pauseOnHover : false,
+            onClosed: function () {window.location.assign('mainApp/index.html');} 
+          });
+    }
+    
+
+});
