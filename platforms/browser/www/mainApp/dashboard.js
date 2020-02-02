49061c604d471f4e12b8b7a399146126a285c532
haxorsprogramming <haxorsuinsu@gmail.com>
2020-02-03 04:46:58 +0700

    blueeming

--- platforms/browser/www/mainApp/dashboard.js
+++ platforms/browser/www/mainApp/dashboard.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
     $('#divUtama').load('beranda.html');
+
+    $('#btnLogOut').click(function(){
+        window.location.assign('../index.html');
+    });
+
+    $('#btnLogOutTop').click(function(){
+        window.location.assign('../index.html');
+    });
+
+    $('.btnUjiDiagnosis').click(function(){
+        togelSidebar();
+        $('#divUtama').html("Memuat ... ");
+    });
+
+    $('.btnDataGejala').click(function(){
+        togelSidebar();
+        $('#divUtama').html("Memuat ... ");
+        $('#divUtama').load('dataGejala.html');
+    });
+
+    function togelSidebar(){
+        $('body').removeClass('sidebar-show');
+        $('body').addClass('sidebar-gone');
+    }
+
 });
